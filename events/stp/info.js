089f7d54deedfb7afa5e1242e44ee0e3d4c06e88
sinβ <149294811+sinBetaKun@users.noreply.github.com>
2024-10-25 16:59:25 +0900

    削除可能なロール

--- events/stp/info.js
+++ events/stp/info.js
@@ -12,6 +12,7 @@ module.exports = {
                 senior : '1289838778827018292',
             },
             ymme : {
+                __allow_remove : true,
                 studying : '1289832840183418941',
                 developer : '1289832456920502354',
             },
